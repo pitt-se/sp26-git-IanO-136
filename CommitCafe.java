fab2a8ba301d20d60dc46051ac36f538bc207e58
Ian O'Leary <ianoleary05@gmail.com>
2026-04-06 13:44:15 -0400

    Brew Function

--- CommitCafe.java
+++ CommitCafe.java
@@ -1,13 +1,17 @@
 public class CommitCafe {
 
-    private static String barista = "Nadine"; // STUDENT_TODO_1A: Change name
+    private static String barista = "Ian"; // STUDENT_TODO_1A: Change name
     // STUDENT_TODO_1B: Add a nickname or title for the barista
+    private static String baristaTitle = "Valued Employee";
 
     private static int cups = 0;
 
     public static void brew(String drink) {
         // STUDENT_TODO_2A: Implement - add 1 to cups & print a message
+        cups++;
+        System.out.println("Brewing Cup!");
         // STUDENT_TODO_2B: Add a second brew-related improvement
+        System.out.println("You are brewing a " + drink);
     }
 
     public static void printSummary() {
